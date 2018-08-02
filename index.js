e704665912d66e6769bea4117d5ba12209a1ab6f
ShrutiSharmaPathak <ss_shruti@yahoo.com>
2018-08-02 17:09:38 +0530

    first commit of index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+/*conversion of celcius to fahrenheit*/
+var temperatureInCelcius=40;
+var celsiusToFahrenheit;
+
+/* celcius to fahrenheit =(celcius *9)/5 +32 */
+celsiusToFahrenheit=(temperatureInCelcius*9)/5+32;
+
+console.log("Temparature of "+ temperatureInCelcius +" celcius, will be "+ celsiusToFahrenheit +" fahrenheit");
+
+/*conversion of fahrenheit to celcius */
+var tempretureInFahrenheit=104;
+var fahrenheitToCelcius;
+
+/*Fahrenheit to Celcius=((fahrenheit -32)*5)/9 */
+fahrenheitToCelcius=((tempretureInFahrenheit -32)*5)/9;
+
+console.log("Temparature of "+ tempretureInFahrenheit +" fahrenheit, will be "+ fahrenheitToCelcius +" celcius");
